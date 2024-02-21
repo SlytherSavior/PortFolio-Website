e35e08c138ade044b5c499555d82aa8d58d8aaf1
Slyther <85357750+SlytherSavior@users.noreply.github.com>
2024-02-21 23:15:47 +0545

    Revert "React branch"

--- index.js
+++
@@ -1,6 +0,0 @@
-import { createRoot } from 'react-dom/client';
-
-document.body.innterHTML = '<div id="app"></div>';
-
-const root = createRoot(document.getElementById('app'));
-root.render( '<h1>  TEST REACT </h1>' );
